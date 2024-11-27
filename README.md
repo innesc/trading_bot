a76045848c38668a449fb6620a3ff31a48fb50ee
Clinton Innes <clinton.innes1@gmail.com>
2024-11-27 11:09:58 -0500

    add account logging and README updates

--- README.md
+++ README.md
@@ -8,3 +8,27 @@ Steps for building docker image
 
 `docker --debug build . -t v2_trade`
 
+
+
+## Sample API string for kraken
+
+Kraken
+`limit_order = trade.create_order(
+                ordertype="market",
+                side="buy",
+                volume=10,
+                pair=coin_kraken)`
+
+coinbase
+
+`rest_client.market_order_buy(client_order_id='00054353401', product_id='BTC-USDC',quote_size='1')`
+
+### Reference links
+
+Kraken API
+https://github.com/btschwertfeger
+
+
+
+Coinbase API
+  https://docs.cdp.coinbase.com/advanced-trade/docs/sdk-rest-client-trade/
--- app/trading_bot.py
+++ app/trading_bot.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import os
 from kraken.spot import  Funding, Market, Trade, User
 import logging
+import json
 import random
 from dotenv import load_dotenv
 load_dotenv()
@@ -24,18 +25,30 @@ def trade_buy_coin(rest_client,
                     volume=0.0001,
                     buffer=0.05,
                     cancel=True):
-    '''
-    https://docs.cdp.coinbase.com/advanced-trade/docs/sdk-rest-client-trade/
-    Works
-    #rest_client.market_order_buy(client_order_id='00054353401', product_id='BTC-USDC',quote_size='1')
-    '''
-   
+    """
+    Place a limit buy order on coinbase using the REST client
+    Parameters:
+    rest_client (RESTClient): Instance of the REST Client
+    count (int): Client order ID
+    price (float, None): Price at which to place the order. If None, use the current price
+    coin_coin (str): The coin pair to trade
+    volume (float): The amount of the coin to buy
+    buffer (float): The buffer to apply to the price
+    cancel (bool): Whether to cancel any existing limit orders after placing this one
+    """
     try:
+        # If price is None, use the current price
+        if price is None:
+            product = rest_client.get_product(coin_coin)
+            price = float(product["price"])
+            price = str(int(((price - buffer*price)//1)))
+
+        # Place the limit order
         limit_order = rest_client.limit_order_gtc_buy(
             client_order_id=str(count),
             product_id=coin_coin,
             base_size=str(volume),
-            limit_price=str(price))
+            limit_price=price)
         print(limit_order)
     except Exception as e:
         # Log the full error details
@@ -46,6 +59,7 @@ def trade_buy_coin(rest_client,
     print(f"value of cancel: {cancel}")
 
     if cancel:
+        # Cancel any existing limit orders
         limit_order_id = limit_order["order_id"]
         rest_client.cancel_orders(order_ids=[limit_order_id])
 
@@ -57,18 +71,26 @@ def trade_buy_kraken(trade,
                     volume=0.0001,
                     buffer=0.05,
                     cancel=True):
+    """
+    Place a limit buy order on Kraken using the kraken.spot.Trade client
+    Parameters:
+    trade (kraken.spot.Trade): Instance of the Trade client
+    price (float, None): Price at which to place the order. If None, use the current price
+    coin_coin (str): The coin pair to trade
+    coin_kraken (str): The coin pair on kraken
+    volume (float): The amount of the coin to buy
+    buffer (float): The buffer to apply to the price
+    cancel (bool): Whether to cancel any existing limit orders after placing this one
     
-    '''
-    Works
-limit_order = trade.create_order(
-                ordertype="market",
-                side="buy",
-                volume=10,
-                pair=coin_kraken,
-            )
-    '''
-
+    """
     try:
+        # If price is None, use the current price
+        if price is None:
+            product = trade.get_product(coin_coin)
+            price = float(product["price"])
+            price = str(int(((price - buffer*price)//1)))
+
+        # Place the limit order
         limit_order = trade.create_order(
                 ordertype="limit",
                 side="buy",
@@ -78,14 +100,36 @@ limit_order = trade.create_order(
             )
         print(limit_order)
     except Exception as e:
-        
+        # Log the full error details
         logger.error(f"Error placing limit order kraken: {e}")
         if hasattr(e, 'response') and e.response is not None:
             logger.error(f"Response content: {e.response.content}")
 
     if cancel:
-        trade.cancel_all_orders()
+        # Cancel any existing limit orders
+        limit_order_id = limit_order["order_id"]
+        trade.cancel_all_orders(order_ids=[limit_order_id])
 
+
+def get_account_balances():
+
+    user = User(key=KRAKEN_API_KEY, secret=KRAKEN_SECRET_KEY)
+    account_balance = user.get_account_balance()
+
+    client = RESTClient(
+                        api_secret=CB_SECRET,
+                        api_key=CB_API_KEY,
+                        base_url='api.coinbase.com'
+                        )
+    accounts = client.get_accounts()['accounts']
+    print(accounts)
+    coin_USDC = [account for account in accounts if (account['name'] in [ 'USDC Wallet'])][0]['available_balance']['value']
+    coin_BTC = [account for account in accounts if (account['name'] in ['BTC Wallet'])][0]['available_balance']['value']
+
+    total_portfolio = pd.DataFrame({'USDC': [float(account_balance['USDC']) + float(coin_USDC)], 'BTC':[float(account_balance['XXBT']) + float(coin_BTC)]})
+    print(total_portfolio)
+
+    return total_portfolio
 
 def sell_kraken(trade,
                 price=None,
@@ -96,9 +140,22 @@ def sell_kraken(trade,
                 cancel=True):
     '''
     Will sell if buy on coin
+    Parameters:
+    trade (kraken.spot.Trade): Instance of the Trade client
+    price (float, None): Price at which to place the sell order. If None, use the current price
+    coin_coin (str): The coin pair to sell
+    coin_kraken (str): The coin pair on kraken
+    volume (float): The amount of the coin to sell
+    buffer (float): The buffer to apply to the price
+    cancel (bool): Whether to cancel any existing limit orders after placing this one
     '''
-
     try:
+        # If price is None, use the current price
+        if price is None:
+            market = trade.get_ticker()
+            price = market[coin_kraken]['c'][0]
+
+        # Place the limit sell order
         limit_order = trade.create_order(
             ordertype='limit',
             side='sell',
@@ -108,12 +165,13 @@ def sell_kraken(trade,
             )
         print(limit_order)
     except Exception as e:
+        # Log the full error details
         logger.error(f"Error placing limit order kraken: {e}")
         if hasattr(e, 'response') and e.response is not None:
             logger.error(f"Response content: {e.response.content}")
 
-
     if cancel:
+        # Cancel any existing limit orders
         trade.cancel_all_orders()
 
 
@@ -153,16 +211,29 @@ def coinbase_scan(rest_client):
     return df
 
 def get_price_kraken(coin):
+    """
+    Gets the current price of a coin on Kraken.
+
+    Parameters
+    ----------
+    coin : str
+        The coin to get the price for. For example, 'BTC/CAD'.
+
+    Returns
+    -------
+    float
+        The current price of the coin on Kraken.
+    """
     market = Market()
 
-    #:return: The ticker(s) including ask, bid, close, volume, vwap, high, low, todays open and more
+    # Get the ticker for the coin
     data = market.get_ticker()
     df = pd.DataFrame(data)
     
-    print("This ran")
     logger.debug(df[coin].loc['a'])
+    logger.debug(df[coin].loc['b'])
 
-
+    # Calculate the mean of the ask and bid prices
     return (float(df[coin].loc['a'][0]) +  float(df[coin].loc['b'][0])) / 2
 
 
@@ -196,7 +267,7 @@ def price_logger(price_krak, price_coin, coin_coin, path_csv='/Users/clintoninne
         
 
 
-def assess(count: int, traded: bool, count_trades: int, threshold=2) -> bool:
+def assess(count: int, traded: bool, count_trades: int, threshold=2, logging_path='/Users/clintoninnes/Desktop/programming/python_stuff/2024/trading_bot/trading_bot_accounts().csv') -> bool:
     """
     Determine if a trade attempt is allowed.
 
@@ -208,6 +279,18 @@ def assess(count: int, traded: bool, count_trades: int, threshold=2) -> bool:
     Returns:
     bool: False if a trade has been attempted, True otherwise.
     """
+    
+
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    if os.path.isfile(logging_path) == False :
+        df =  get_account_balances()
+        df.to_csv(logging_path, index=False)
+    else:
+        df_all = pd.read_csv(logging_path)
+        df =  get_account_balances()
+        df.to_csv(logging_path, index=False)
+        
+
     # Check if a trade has already been made
     if (traded & (count_trades > threshold)):
         # No further trade attempts allowed
@@ -217,26 +300,28 @@ def assess(count: int, traded: bool, count_trades: int, threshold=2) -> bool:
         return True
     
 def orchestration(
-                    buffer=0.05,
-                    volume=0.0001,
-                    coin_coin='BTC-USDC',
-                    kraken_coin='BTC/USDC',
-                    count=1,
-                    kraken_market='TBTCUSD',
-                    count_trades=0
-                    ):
+                    buffer: float = 0.05,
+                    volume: float = 0.0001,
+                    coinbase_coin: str = 'BTC-USDC',
+                    kraken_coin: str = 'BTC/USDC',
+                    count: int = 1,
+                    kraken_market: str = 'TBTCUSD',
+                    count_trades: int = 0
+                    ) -> tuple:
     """
-    Orchestration function for trading bot
+    Orchestration function for trading bot. This function will trade if the price of the coin on Kraken is higher than the
+    price of the coin on Coinbase, and the difference is bigger than the buffer.
+
     Parameters:
     buffer (float): how much more expensive the kraken price must be than the coin price
     volume (float): how many coins to trade
-    coin_coin (str): id of the coin on coinbase
+    coinbase_coin (str): id of the coin on coinbase
     kraken_coin (str): id of the coin on kraken
     count (int): a counter to keep track of the trade number
     kraken_market (str): market to get the price from in kraken
     
     Returns:
-    bool: whether a trade was made or not
+    tuple: whether a trade was made or not, and the new count_trades
     """
     rest_client = RESTClient(
                         api_secret=CB_SECRET,
@@ -247,74 +332,54 @@ def orchestration(
     trade = Trade(key=KRAKEN_API_KEY, secret=KRAKEN_SECRET_KEY )
 
     # Get the price from Kraken
-    price_krak = get_price_kraken( kraken_market)
-    logger.info(f"The price in kraken {price_krak}")
+    kraken_price = get_price_kraken(kraken_market)
+    logger.info(f"The price in kraken {kraken_price}")
 
     # Get the price from Coinbase
-    price_coin = rest_client.get_products()
-    df = pd.DataFrame(price_coin['products'])
-    price_coin = df[df['product_id']==coin_coin]['price']
-    logger.info(f"The price in coin {price_coin}")
+    coinbase_price = rest_client.get_products()['products'][coinbase_coin]['price']
+    logger.info(f"The price in coin {coinbase_price}")
 
-    price_krak = float(price_krak )
-    price_coin= float(price_coin)
-    print(f"price coin: {price_coin}  price kraken {price_krak}")
-
-    
+    # Check if we should trade
     traded = False
-    '''
-    if price_krak > (price_coin + buffer * price_coin ):
-
-        print("buy coin sell kraken")
+    if kraken_price > (coinbase_price + buffer * coinbase_price):
         # Buy on Kraken
         sell_kraken(trade,
-                price=price_krak,
-                coin_coin=coin_coin,
-                coin_kraken=kraken_coin,
-                volume=volume,
-                buffer=0,
-                cancel=False)
-        
-        print("buy coin worked")
-        # Buy on Coinbase
-        trade_buy_coin(rest_client,
-                       count,
-                    price=price_coin,
-                    coin_coin=coin_coin,
-                    volume=volume,
-                    buffer=0,
-                    cancel=False)
-        print("buy coin worked 2")
-        traded = True
-        count_trades = count_trades + 1
-    elif (price_krak +buffer * price_krak) < price_coin :
-        print("buy kraken sell coin")
-        # Buy on Kraken
-        trade_buy_kraken(trade,
-                    price=price_krak,
-                    coin_coin=coin_coin,
+                    price=kraken_price,
+                    coin_coin=coinbase_coin,
                     coin_kraken=kraken_coin,
                     volume=volume,
                     buffer=0,
                     cancel=False)
-        print("buy kraken sell coin")
-
-        print("sell coin worked")
+        
+        # Buy on Coinbase
+        trade_buy_coin(rest_client,
+                       count,
+                    price=coinbase_price,
+                    coin_coin=coinbase_coin,
+                    volume=volume,
+                    buffer=0,
+                    cancel=False)
+        traded = True
+        count_trades += 1
+    elif (kraken_price + buffer * kraken_price) < coinbase_price:
+        # Buy on Kraken
+        trade_buy_kraken(trade,
+                    price=kraken_price,
+                    coin_coin=coinbase_coin,
+                    coin_kraken=kraken_coin,
+                    volume=volume,
+                    cancel=False)
         # Sell on Coinbase
-        sell_coin(rest_client, count, volume=volume,coin_coin=coin_coin, price=price_coin)
-        print("sell coin worked 2")
+        sell_coin(rest_client, count, volume=volume,coin_coin=coinbase_coin, price=coinbase_price)
         traded = True
         count_trades = count_trades + 1
-    '''
+    
     price_logger(price_krak, price_coin, coin_coin)
     return assess(count, traded, count_trades), count_trades
 
 if __name__ == "__main__":
-    #Docker up --env-file .env
     
     print('test I can change again')
-    #print(CB_API_KEY)
-    #print(CB_SECRET)
 
 
     RUN = True
@@ -324,9 +389,9 @@ if __name__ == "__main__":
     while RUN:
         count += 1
         RUN, count_trades = orchestration(
-                    buffer=0.01,
+                    buffer=0.02,
                     volume=0.0001,
-                    coin_coin='BTC-USDC',
+                    coinbase_coin='BTC-USDC',
                     kraken_coin='BTC/USDC',
                     count=count,
                     count_trades=count_trades
