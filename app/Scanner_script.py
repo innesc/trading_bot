69106e369ab0c6fc25024086863f64870372c927
Clinton Innes <clinton.innes1@gmail.com>
2024-11-27 17:50:41 -0500

    add fix to scanner script

--- app/Scanner_script.py
+++ app/Scanner_script.py
@@ -20,11 +20,14 @@ def kraken_scan():
     #:return: The ticker(s) including ask, bid, close, volume, vwap, high, low, todays open and more
     data = market.get_ticker()
     df = pd.DataFrame(data)
+    for col in df.columns:
+        df[col] = df[col].map(lambda x: x[0])
 
-    df = (df.loc['a',:] + df.loc['b',:]) / 2
+
+    df = ((df.loc['a',:].astype(float) + df.loc['b',:].astype(float)) / 2)
 
     df['logging time kraken'] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    return df
+    return df.to_frame().T
 
 def coinbase_scan():
     """scans for the price of the coins listed in coins_list 
@@ -38,6 +41,8 @@ def coinbase_scan():
 
     price = rest_client.get_products()
     df = pd.DataFrame(price['products'])
+    df.index = df.product_id
+    df = df['price'].to_frame().T
     now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     df['logging_time'] = now
 
@@ -49,8 +54,12 @@ def log_both():
     df_kraken.columns = [col + '_from_kraken' for col in df_kraken.columns]
     df_coinbase.columns = [col + '_from_coin' for col in df_coinbase.columns]
 
+    print( df_coinbase.shape)
 
-    return pd.Concat([df_kraken, df_coinbase], axis=0)
+    print(df_kraken.shape)
+
+
+    return pd.concat([df_kraken, df_coinbase], axis=1)
 
 
 
@@ -67,9 +76,10 @@ if __name__ == "__main__":
         Data = log_both()
 
     while RUN:
-        time.sleep(2)
+        time.sleep(30)
         df = log_both()
         Data = pd.concat([Data,df])
         Data.to_csv(PATH)
+        print(f"This many rows and columns logged {str(Data.shape)}")
 
 
